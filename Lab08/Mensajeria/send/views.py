5e32d92671ed958c0c4f4e5acff11f7ee1d8f21a
leonhatches <jhatches@unsa.edu.pe>
2025-06-08 15:03:11 -0500

    | Laboratorio 08 | Mensajeria | Views | : Se crea la view para enviar un correo de mi personal al correo institucional

--- Lab08/Mensajeria/send/views.py
+++ Lab08/Mensajeria/send/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 def index(request):
+    send_mail(
+        'Hello from Django',
+        'Hello there, this is an automated message.',
+        '***@gmail.com',
+        ['***@unsa.edu.pe'],
+        fail_silently = False
+    )
+    
     return render(request, 'send/index.html', {})
