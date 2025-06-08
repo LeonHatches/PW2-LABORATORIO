3c5f53132a458a0296f8bfbb613facdc15655146
leonhatches <jhatches@unsa.edu.pe>
2025-06-08 11:48:30 -0500

    | Laboratorio 08 | Conversor | Views | : Se define cuando la vista cuando ingrese por get(abre la pag) y el formulario a través del post

--- Lab08/Conversor/Conversor/views.py
+++ Lab08/Conversor/Conversor/views.py
@@ -1,26 +1,36 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.views.generic import View
 from django.template.loader import get_template
 from .renders import render_to_pdf
 
 class GeneratePDF(View):
     def get(self, request, *args, **kwargs):
+        return render(request, 'pdf/index.html', {})
+    
+    def post(self, request, *args, **kwargs):
         template = get_template('pdf/invoice.html')
+        
+        file  = request.POST.get('filename')
+        title = request.POST.get('title')
+        text  = request.POST.get('text')
+        download = request.POST.get('download')
+        
         context = {
-            "invoice_id": 123,
-            "customer_name": "Leon Hatches",
-            "amount": 1399.00,
-            "today": "today",
+            "title": title,
+            "text": text,
         }
+
         html = template.render(context)
         pdf = render_to_pdf('pdf/invoice.html', context)
+        
         if pdf:
             response = HttpResponse(pdf, content_type='application/pdf')
-            filename = "Invoice_%s.pdf" %("prueba")
-            content = "inline; filename='%s'" %(filename)
-            download = request.GET.get("download")
+            filename = '%s.pdf' %(file)
+            content = "inline; filename=%s" %(filename)
+
             if download:
-                content = "attachment; filename='%s'" %(filename)
+                content = "attachment; filename=%s" %(filename)
             
             response['Content-Disposition'] = content
             return response
