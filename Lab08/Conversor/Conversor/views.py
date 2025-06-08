58f0bf825b7ef4b4afc9ce30f225fc2c0b8ab6ae
leonhatches <jhatches@unsa.edu.pe>
2025-06-08 07:44:26 -0500

    | Laboratorio 08 | Conversor | Views | : Se crea la view del video

---
+++ Lab08/Conversor/Conversor/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.views.generic import View
+from django.template.loader import get_template
+from .renders import render_to_pdf
+
+class GeneratePDF(View):
+    def get(self, request, *args, **kwargs):
+        template = get_template('invoice.html')
+        context = {
+            "invoice_id": 123,
+            "customer_name": "Leon Hatches",
+            "amount": 1399.00,
+            "today": "today",
+        }
+        html = template.render(context)
+        return HttpResponse(html)
