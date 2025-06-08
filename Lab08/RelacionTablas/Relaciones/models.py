2c3d76786314f5c3d110a98a63d63f195e73a12e
leonhatches <jhatches@unsa.edu.pe>
2025-06-08 06:34:11 -0500

    | Laboratorio 08 | RelacionTablas | Models | : Se crea el atributo de movies

--- Lab08/RelacionTablas/Relaciones/models.py
+++ Lab08/RelacionTablas/Relaciones/models.py
@@ -16,12 +16,14 @@ class Framework (models.Model):
 
 class Movie (models.Model):
     name = models.CharField(max_length = 10)
+    characters
 
     def __str__(self):
         return self.name
 
 class Character (models.Model):
     name = models.CharField(max_length = 10)
+    movies = models.ManyToManyField(Movie)
 
     def __str__(self):
         return self.name
